5cf275272dbc91470cbf2d37865acc5a414e130d
User <prrvchr@gmail.com>
2020-02-01 18:43:16 +0100

    git subrepo pull ./CloudUcpOOo
    subrepo:
      subdir:   "CloudUcpOOo"
      merged:   "fac0486"
    upstream:
      origin:   "https://github.com/prrvchr/CloudUcpOOo.git"
      branch:   "master"
      commit:   "fac0486"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

--- CloudUcpOOo/python/clouducp/dbinit.py
+++ CloudUcpOOo/python/clouducp/dbinit.py
@@ -18,59 +18,40 @@ from .dbtools import getDataSourceConnection
 from .dbtools import createDataSource
 from .dbtools import checkDataBase
 from .dbtools import createStaticTable
-from .logger import logMessage
 
 import traceback
 
 
 def getDataSourceUrl(ctx, dbname, plugin, register):
-    try:
-        error = None
-        url = getResourceLocation(ctx, plugin, g_path)
-        odb = '%s/%s.odb' % (url, dbname)
-        if not getSimpleFile(ctx).exists(odb):
-            dbcontext = ctx.ServiceManager.createInstance('com.sun.star.sdb.DatabaseContext')
-            datasource = createDataSource(dbcontext, url, dbname)
-            error = _createDataBase(ctx, datasource, url, dbname)
-            logMessage(ctx, INFO, "Stage 5", 'dbinit', 'getDataSourceUrl()')
-            if error is None:
-                datasource.DatabaseDocument.storeAsURL(odb, ())
-                if register:
-                    logMessage(ctx, INFO, "Stage 6", 'dbinit', 'getDataSourceUrl()')
-                    registerDataSource(dbcontext, dbname, odb)
-        logMessage(ctx, INFO, "Stage 7", 'dbinit', 'getDataSourceUrl()')
-        return url, error
-    except Exception as e:
-        msg = "getDataSourceUrl: ERROR: %s - %s" % (e, traceback.print_exc())
-        logMessage(ctx, SEVERE, msg, 'dbinit', 'getDataSourceUrl()')
+    error = None
+    url = getResourceLocation(ctx, plugin, g_path)
+    odb = '%s/%s.odb' % (url, dbname)
+    if not getSimpleFile(ctx).exists(odb):
+        dbcontext = ctx.ServiceManager.createInstance('com.sun.star.sdb.DatabaseContext')
+        datasource = createDataSource(dbcontext, url, dbname)
+        error = _createDataBase(ctx, datasource, url, dbname)
+        if error is None:
+            datasource.DatabaseDocument.storeAsURL(odb, ())
+            if register:
+                registerDataSource(dbcontext, dbname, odb)
+    return url, error
 
 def _createDataBase(ctx, datasource, url, dbname):
-    #connection, error = getDataSourceConnection(datasource)
-    logMessage(ctx, INFO, "Stage 1", 'dbinit', '_createDataBase()')
     error = None
     try:
         connection = datasource.getConnection('', '')
     except SQLException as e:
         error = e
-    logMessage(ctx, INFO, "Stage 2", 'dbinit', '_createDataBase()')
-    if error is not None:
-        logMessage(ctx, INFO, "Stage 3", 'dbinit', '_createDataBase()')
-        return error
-    logMessage(ctx, INFO, "Stage 4", 'dbinit', '_createDataBase()')
-    version, error = checkDataBase(connection)
-    logMessage(ctx, INFO, "Stage 5", 'dbinit', '_createDataBase()')
-    if error is None:
-        logMessage(ctx, INFO, "Stage 6", 'dbinit', '_createDataBase()')
-        statement = connection.createStatement()
-        logMessage(ctx, INFO, "Stage 7", 'dbinit', '_createDataBase()')
-        createStaticTable(statement, _getStaticTables(), True)
-        tables, statements = getTablesAndStatements(statement, version)
-        executeSqlQueries(statement, tables)
-        logMessage(ctx, INFO, "Stage 8", 'dbinit', '_createDataBase()')
-        executeQueries(statement, _getViews())
-    connection.close()
-    connection.dispose()
-    logMessage(ctx, INFO, "Stage 9", 'dbinit', '_createDataBase()')
+    else:
+        version, error = checkDataBase(ctx, connection)
+        if error is None:
+            statement = connection.createStatement()
+            createStaticTable(statement, _getStaticTables(), True)
+            tables, statements = getTablesAndStatements(statement, version)
+            executeSqlQueries(statement, tables)
+            executeQueries(statement, _getViews())
+        connection.close()
+        connection.dispose()
     return error
 
 def _getStaticTables():
