907653219d395617ef1e64b0b7408db2a44022e7
User <prrvchr@gmail.com>
2020-06-08 16:04:24 +0200

    New Wizard for LO 6.4.x and 7.x

--- CloudUcpOOo/make_rdb.sh
+++ CloudUcpOOo/make_rdb.sh
@@ -6,7 +6,6 @@ Path=$(dirname "${0}")
 rm ${Path}/types.rdb
 
 ./rdb/make_rdb.sh com/sun/star/auth/XRestKeyMap
-./rdb/make_rdb.sh com/sun/star/auth/XOAuth2Request
 ./rdb/make_rdb.sh com/sun/star/auth/XInteractionUserName
 ./rdb/make_rdb.sh com/sun/star/auth/RestRequestTokenType
 ./rdb/make_rdb.sh com/sun/star/ucb/RestDataSourceSyncMode
