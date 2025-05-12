a3be956edb867474d2ce82eac7c767e0a2bb31d3
prrvchr <prrvchr@gmail.com>
2025-05-12 19:01:58 +0200

    git subrepo pull uno
    subrepo:
      subdir:   "uno"
      merged:   "0411c787"
    upstream:
      origin:   "https://github.com/prrvchr/uno.git"
      branch:   "main"
      commit:   "0411c787"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"

---
+++ uno/lib/uno/ucb/dispatch.py
@@ -0,0 +1,121 @@
+#!
+# -*- coding: utf-8 -*-
+
+"""
+╔════════════════════════════════════════════════════════════════════════════════════╗
+║                                                                                    ║
+║   Copyright (c) 2020-25 https://prrvchr.github.io                                  ║
+║                                                                                    ║
+║   Permission is hereby granted, free of charge, to any person obtaining            ║
+║   a copy of this software and associated documentation files (the "Software"),     ║
+║   to deal in the Software without restriction, including without limitation        ║
+║   the rights to use, copy, modify, merge, publish, distribute, sublicense,         ║
+║   and/or sell copies of the Software, and to permit persons to whom the Software   ║
+║   is furnished to do so, subject to the following conditions:                      ║
+║                                                                                    ║
+║   The above copyright notice and this permission notice shall be included in       ║
+║   all copies or substantial portions of the Software.                              ║
+║                                                                                    ║
+║   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,                  ║
+║   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES                  ║
+║   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.        ║
+║   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY             ║
+║   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,             ║
+║   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE       ║
+║   OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                    ║
+║                                                                                    ║
+╚════════════════════════════════════════════════════════════════════════════════════╝
+"""
+
+import uno
+import unohelper
+
+from com.sun.star.ui.dialogs.TemplateDescription import FILEOPEN_SIMPLE
+from com.sun.star.ui.dialogs.TemplateDescription import FILESAVE_SIMPLE
+
+from com.sun.star.frame import FeatureStateEvent
+
+from com.sun.star.frame import XNotifyingDispatch
+
+from com.sun.star.frame.DispatchResultState import SUCCESS
+from com.sun.star.frame.DispatchResultState import FAILURE
+
+from .unotool import createService
+from .unotool import getArgumentSet
+from .unotool import getDesktop
+from .unotool import getMessageBox
+
+import traceback
+
+
+class Dispatch(unohelper.Base,
+               XNotifyingDispatch):
+    def __init__(self, ctx, frame):
+        self._ctx = ctx
+        self._frame = frame
+        self._listeners = []
+        self._service = 'com.sun.star.ui.dialogs.OfficeFilePicker'
+        self._sep = '/'
+
+    _path = ''
+
+# XNotifyingDispatch
+    def dispatchWithNotification(self, url, arguments, listener):
+        state, result = self.dispatch(url, arguments)
+        struct = 'com.sun.star.frame.DispatchResultEvent'
+        notification = uno.createUnoStruct(struct, self, state, result)
+        listener.dispatchFinished(notification)
+
+    def dispatch(self, url, arguments):
+        state = FAILURE
+        result = None
+        if url.Path == 'Open':
+            fp = createService(self._ctx, self._service, FILEOPEN_SIMPLE)
+            fp.setDisplayDirectory(Dispatch._path)
+            fp.setMultiSelectionMode(True)
+            urls = ()
+            if fp.execute():
+                urls = fp.getSelectedFiles()
+                Dispatch._path = fp.getDisplayDirectory()
+                state = SUCCESS
+            fp.dispose()
+            if state == SUCCESS:
+                desktop = getDesktop(self._ctx)
+                for url in urls:
+                    desktop.loadComponentFromURL(url, '_default', 0, ())
+        elif url.Path == 'SaveAs':
+            document = self._frame.getController().getModel()
+            source = document.getURL()
+            path, _, name = source.rpartition(self._sep)
+            fp = createService(self._ctx, self._service, FILESAVE_SIMPLE)
+            fp.setDisplayDirectory(path + self._sep)
+            fp.setDefaultName(name)
+            if fp.execute():
+                target = fp.getSelectedFiles()[0]
+                Dispatch._path = fp.getDisplayDirectory()
+                if source != target:
+                    document.storeAsURL(target, ())
+                else:
+                    document.store()
+                state = SUCCESS
+            fp.dispose()
+        elif url.Path == 'ShowWarning':
+            window = self._frame.getContainerWindow().getToolkit().getActiveTopWindow()
+            msgbox = getMessageBox(window, **getArgumentSet(arguments))
+            msgbox.execute()
+            msgbox.dispose()
+            state = SUCCESS
+        return state, result
+
+    def addStatusListener(self, listener, url):
+        state = FeatureStateEvent()
+        state.FeatureURL = url
+        state.IsEnabled = True
+        #state.State = True
+        listener.statusChanged(state)
+        self._listeners.append(listener);
+
+    def removeStatusListener(self, listener, url):
+        if listener in self._listeners:
+            self._listeners.remove(listener)
+
